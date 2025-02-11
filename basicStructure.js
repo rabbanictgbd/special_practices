e671c78981aedb74f8c461d59a547bdcc022e254
Abdul Kader Rabbani <rabbanictgbd@gmail.com>
2025-02-11 16:33:41 +0600

    structure

--- basicStructure.js
+++ basicStructure.js
@@ -1,2 +1,29 @@
+// console log : javaScript basic structure.............................................
 console.log()
 
+// variable for javaScript...............................................................
+var name= "The name" // generally var then the variable name then "=" equal sign then value
+var number= 25
+const theName= "The string value"
+const theNumber= 30
+let theNameWillChange= "Changeable name"
+let theNumberWillChange= 100
+
+//  if, else if condition.................................................................
+if(10==15){
+    console.log("10 equal 10 is true")
+} 
+else if(100<50){
+console.log("The condition 1")
+}
+else if(50<50){
+    console.log("The condition 2")
+}
+else if(10<50){
+console.log("The condition 3")
+}
+else {
+console.log("All the conditions are wrong.")
+}
+   
+// for loop .............................................................................................
--- condition.js
+++ condition.js
@@ -1,5 +1,85 @@
+
+// Normal if true condition.................................................................
+if(10==10){
+    console.log("10 equal 10 is true")
+} 
+// Normal if false condition.................................................................
+if(10==15){
+    console.log("10 equal 10 is true")
+} 
+
+// Normal if else condition.................................................................
 if(10==15){
     console.log("10 equal 10 is true")
 } else{
     console.log("The condition is not true")
 }
+
+//  if else if true condition.................................................................
+if(10==10){
+    console.log("10 equal 10 is true")
+} 
+else if(100<50){
+console.log("The condition 1")
+}
+else if(50<50){
+    console.log("The condition 2")
+}
+else if(10<50){
+console.log("The condition 3")
+}
+else {
+console.log("All the conditions are wrong.")
+}
+    
+//  if else if mid true condition.................................................................
+if(10==15){
+    console.log("10 equal 10 is true")
+} 
+else if(100<50){
+console.log("The condition 1")
+}
+else if(50<50){
+    console.log("The condition 2")
+}
+else if(10<50){
+console.log("The condition 3")
+}
+else {
+console.log("All the conditions are wrong.")
+}
+   
+//  if else if false condition.................................................................
+if(10==15){
+    console.log("10 equal 10 is true")
+} 
+else if(100<50){
+console.log("The condition 1")
+}
+else if(50<50){
+    console.log("The condition 2")
+}
+else if(50<50){
+console.log("The condition 3")
+}
+else {
+console.log("All the conditions are wrong.")
+}
+   
+//  if else if true multiple mid, working first true condition.................................................................
+if(10==15){
+    console.log("10 equal 10 is true")
+} 
+else if(100<50){
+console.log("The condition 1")
+}
+else if(40<50){
+    console.log("The condition 2")
+}
+else if(10<50){
+console.log("The condition 3")
+}
+else {
+console.log("All the conditions are wrong.")
+}
+   
